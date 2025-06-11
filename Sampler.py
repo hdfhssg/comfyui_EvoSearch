54e70407c8f657a8ff97e6e06d24dfb6b1732726
PengXin <78085965+hdfhssg@users.noreply.github.com>
2025-06-11 18:28:55 +0800

    Update Sampler.py

--- Sampler.py
+++ Sampler.py
@@ -2,7 +2,32 @@ import torch
 from comfy.samplers import KSampler
 # 假设 EvoSearch 库中提供了如下函数 
 from .evosearch.utils import do_eval 
-from comfy.sample import common_ksampler
+
+def common_ksampler(model, seed, steps, cfg, sampler_name, scheduler, positive, negative, latent, denoise=1.0, 
+                    disable_noise=False, start_step=None, last_step=None, force_full_denoise=False,
+                    if_pyramid_noise=False,device = "cpu", discount=0.2, max_layers=6,mode = "add",T=1.0):
+    latent_image = latent["samples"]
+    latent_image = comfy.sample.fix_empty_latent_channels(model, latent_image)
+
+    if disable_noise:
+        noise = torch.zeros(latent_image.size(), dtype=latent_image.dtype, layout=latent_image.layout, device="cpu")
+    else:
+        batch_inds = latent["batch_index"] if "batch_index" in latent else None
+        noise = prepare_noise(latent_image, seed, batch_inds, if_pyramid_noise,device, discount, max_layers,mode,T)
+
+    noise_mask = None
+    if "noise_mask" in latent:
+        noise_mask = latent["noise_mask"]
+
+    callback = latent_preview.prepare_callback(model, steps)
+    disable_pbar = not comfy.utils.PROGRESS_BAR_ENABLED
+    samples = comfy.sample.sample(model, noise, steps, cfg, sampler_name, scheduler, positive, negative, latent_image,
+                                  denoise=denoise, disable_noise=disable_noise, start_step=start_step, last_step=last_step,
+                                  force_full_denoise=force_full_denoise, noise_mask=noise_mask, callback=callback, disable_pbar=disable_pbar, seed=seed)
+    out = latent.copy()
+    out["samples"] = samples
+    return (out, )
+
 import numpy as np
 class EvoSearch_FLUX:
     CATEGORY = "evolution"
